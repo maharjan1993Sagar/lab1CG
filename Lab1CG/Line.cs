a46906471cc2298d773c2935caed8957d8420e9c
Your Name <maharjanakancha@gmail.com>
2019-11-13 07:20:30 +0545

    line

---
+++ Lab1CG/Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1CG
+{
+    class Line
+    {
+       public Point Point1=new Point();
+      public Point Point2= new Point();
+        
+    }
+}
---
+++ Lab1CG/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1CG
+{
+    class Point
+    {
+     public int x, y;
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+}
