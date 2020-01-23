a51621f04a07d303739d86d937d71c8ba2643176
Your Name <maharjanakancha@gmail.com>
2020-01-23 09:08:23 +0545

    lab 4

---
+++ Lab1CG/ExtremePoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1CG
+{
+    class ExtremePoints
+    {
+        public Point Point { get; set; }
+        public bool IsExtreme { get; set; } = true;
+    }
+}
---
+++ Lab1CG/SortPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1CG
+{
+   public class SortPoint
+    {
+        public double Angle { get; set; }
+        public Point Point { get; set; }
+    }
+}
